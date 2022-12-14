9e02ed612341c7dfaf7d92a9e05c8770f9182f31
agabrie <agabriels@2u.com>
2022-12-14 10:47:59 +0200

    added jquery functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,18 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 function generateBlobs() {
 	let num_blobs = getRandomInteger(10, 30);
-	let floating_shapes = document.getElementById("floating-shapes");
+	let floating_shapes = $("#floating-shapes");
 	for (let i = 0; i <= num_blobs; i++) {
 		let x = getRandomInteger(0, 100) - 10;
 		let y = getRandomInteger(0, 100) - 10;
 		let size = getRandomInteger(5, 40);
 		let blob_element = createBlob(x, y, size);
-		floating_shapes.appendChild(blob_element);
+		floating_shapes.append(blob_element);
 	}
 }
 function createBlob(x, y,size) {
-	let blob_element = document.createElement("div");
-	blob_element.classList.add("blob");
+	let blob_element = $(`<div class="blob"></div>`);
 	let rotation = getRandomInteger(0, 360);
 	let r = getRandomInteger(0, 255);
 	let g = getRandomInteger(0, 255);
@@ -22,33 +21,29 @@ function createBlob(x, y,size) {
 	let a = getRandomInteger(1, 9) / 10;
 	let alpha = getRandomInteger(0, 4) / 10;
 	let anim_duration = getRandomInteger(3, 10);
-	blob_element.style.top = `${y}%`;
-	blob_element.style.left = `${x}%`;
-	blob_element.style.width = `${size}em`;
-	blob_element.style.transform = `rotate(${rotation}deg)`;
-	blob_element.style.boxShadow = `1px 1px 10px rgb(0 0 0 / ${alpha})`;
-	blob_element.style.backgroundColor = `rgba(${r},${g},${b},${a})`;
-	blob_element.style.animation = `contort ${anim_duration}s linear 0s infinite alternate`;
+	blob_element.css({
+		top:`${y}%`,
+		left: `${x}%`,
+		width : `${size}em`,
+		transform : `rotate(${rotation}deg)`,
+		boxShadow : `1px 1px 10px rgb(0 0 0 / ${alpha})`,
+		backgroundColor : `rgba(${r},${g},${b},${a})`,
+		animation : `contort ${anim_duration}s linear 0s infinite alternate`,
+	})
 	return blob_element;
 }
 function getRandomInteger(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let embed = document.getElementById("project-preview");
+let embed = $("#project-preview");
 
-console.log(embed)
+let btnView = $("#btn-project-view");
 
-let btnView = document.getElementById("btn-project-view");
-console.log(btnView)
-
-btnView.addEventListener('click',(e)=>{
-	let projectParent = e.target.parentElement.parentElement;
-	let projectDescription = projectParent.children[1]
-	let projectAnchor = projectDescription.children[0]
-	
-	let url = projectAnchor.href
-	console.log(url, embed.src)
-	embed.src = url;
-	embed.removeAttribute("hidden")
+btnView.on('click',function(){
+	let projectParent = $(this).parents(".project-container");
+	let projectDescription = projectParent.find(".project-name")
+	let url = projectDescription.attr("href");
+	embed.attr("src",url);
+	embed.removeAttr("hidden")
 })
